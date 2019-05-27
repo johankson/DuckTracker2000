e1614475558ff9cbc1f89fe198c37436e4298dac
Johan Karlsson <johan.kson@gmail.com>
2019-05-27 21:07:13 +0200

    Swipe example done

---
+++ DuckTracker2000/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DuckTracker2000
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NotifyButton.Clicked += NotifyButton_Clicked;
+
+            var swipeGestureRecognizer = new SwipeGestureRecognizer()
+            {
+                Direction = SwipeDirection.Down
+            };
+
+            swipeGestureRecognizer.Swiped += SwipeGestureRecognizer_Swiped;
+            NotificationUI.GestureRecognizers.Add(swipeGestureRecognizer);
+        }
+
+        private async void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            await HideNotification();
+        }
+
+        private async void NotifyButton_Clicked(object sender, EventArgs e)
+        {
+            if (NotificationUI.IsVisible == false)
+            {
+                await ShowNotification("Ducks found!");
+            }
+            else
+            {
+                await HideNotification();
+            }
+        }
+
+        private async Task ShowNotification(string text)
+        {
+            NotificationText.Text = text;
+            NotificationUI.TranslationY = this.Height;
+            NotificationUI.IsVisible = true;
+            await NotificationUI.TranslateTo(0, 400, 250, Easing.SpringIn);
+        }
+
+        private async Task HideNotification()
+        {
+            NotificationUI.IsVisible = true;
+            await NotificationUI.TranslateTo(0, this.Height, 250, Easing.SpringOut);
+            NotificationUI.IsVisible = false;
+        }
+    }
+}
